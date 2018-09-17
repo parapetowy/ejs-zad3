b8096b86bb149046487cbd3a2d2a3a2886387bd5
Hubert Hadyński <hubert.hadynski@gmail.com>
2018-09-17 12:45:50 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+app.use(express.static('assets'));
+
+app.get('/', function (req, res) {
+    res.sendFile('/index.html');
+});
+
+app.get('/userform', function (req, res) {
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.end(JSON.stringify(response));
+});
+
+app.use('/store' ,function(req, res, next){
+    console.log('Jestem pośrednikiem przy żądaniu do /store');
+    next();
+});
+
+app.get('/store', function (req, res) {
+    console.log('Otrzymałem żądanie GET do /store');
+    res.send('To jest sklep');
+});
+
+var server = app.listen(3000, 'localhost', function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+});
+
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
